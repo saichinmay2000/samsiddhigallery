b5d9e423e91a9831be64c2e16c49d7a437f828b8
saichinmay2000 <47316906+saichinmay2000@users.noreply.github.com>
2019-11-06 12:21:11 +0530

    Changed Images in the gallery

--- controller.js
+++ controller.js
@@ -1,4 +1,4 @@
 module.exports.home = (req,res,next)=>{
-    var paths = ['images/a.jpg','images/b.jpg','images/c.jpg'];
+    var paths = ['images/01.jpg','images/02.jpg','images/03.jpg','images/04.jpg','images/05.jpg'];
     res.render('index',{imgs: paths, layout:false});
 };
