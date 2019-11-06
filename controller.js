01b3ace4693d850e3fd1ae7df23e2b24b9fe8cb5
saichinmay2000 <47316906+saichinmay2000@users.noreply.github.com>
2019-11-06 20:34:43 +0530

    Update controller.js

--- controller.js
+++ controller.js
@@ -1,4 +1,4 @@
 module.exports.home = (req,res,next)=>{
-    var paths = ['images/01.jpg','images/02.jpg','images/edited 03.jpg','images/04.jpg','images/05.jpg','images/code6.jpg','images/code4.jpg','images/code3.jpg','images/code7.jpg','images/dan1.jpg','images/dan2.jpg','images/soc1.jpg','images/soc2.jpg','images/sco3.jpg'];
+    var paths = ['images/01.jpg','images/02.jpg','images/edited 03.jpg','images/04.jpg','images/05.jpg','images/code6.jpg','images/code4.jpg','images/code2.jpg','images/code7.jpg','images/dan1.jpg','images/dan2.jpg','images/soc1.jpg','images/soc2.jpg','images/sco3.jpg'];
     res.render('index',{imgs: paths, layout:false});
 };
